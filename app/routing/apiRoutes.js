2e82cb7c4a494ec5b2b98733df34714c8ecb7cff
Noah Sylwester <nanoabram@comcast.net>
2019-10-09 02:00:05 -0700

    copy htmlRoutes code into apiRoutes, add anchor tag to home.html to move to survey.html on button click

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,15 @@
-module.exports = 1;
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+// Basic route that sends the user first to the AJAX Page
+router.get("/", function(req, res) {
+  res.sendFile(path.join(`${__dirname}/../public/`, "home.html"));
+});
+//FriendFinder/app/routing/app/public/home.html'
+
+router.get("/survey", function(req, res) {
+  res.sendFile(path.join(`${__dirname}/../public/`, "survey.html"));
+});
+
+module.exports = router;
