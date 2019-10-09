1f0df6cc308f912cd5545a1a91d11a36e5094e20
Noah Sylwester <nanoabram@comcast.net>
2019-10-09 01:54:21 -0700

    alter static express serving, add survey.html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,8 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(`${__dirname}/app/public/`, "home.html"));
+router.get("/", function(req, res) {
+  res.sendFile(path.join(`${__dirname}/../public/`, "home.html"));
+});
+//FriendFinder/app/routing/app/public/home.html'
+
+router.get("/survey", function(req, res) {
+  res.sendFile(path.join(`${__dirname}/../public/`, "survey.html"));
 });
 
-app.get("/survey", function(req, res) {
-  res.sendFile(path.join(`${__dirname}/app/public/`, "survey.html"));
-});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 
-const apiRoutes = require('/app/routing/apiRoutes.js');
-const htmlRoutes = require('/app/routing/htmlRoutes.js');
+const apiRoutes = require('./app/routing/apiRoutes.js');
+const htmlRoutes = require('./app/routing/htmlRoutes.js');
 
 // Sets up the Express App
 // =============================================================
@@ -10,9 +10,11 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+app.use('/', htmlRoutes);
 
+app.use(express.static(__dirname + '/assets/images/'));
 
 // Starts the server to begin listening
 // =============================================================
