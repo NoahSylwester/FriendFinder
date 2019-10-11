330f67efb5ee9f315863b3eacf69817fdb3a01ff
Noah Sylwester <nanoabram@comcast.net>
2019-10-10 20:58:00 -0700

    update style, add monster friend profiles.

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# SquireFinder
+# Monstrous
 
-For finding friends, medievally.
-
+Because it's hard to meet people when you're a terrifying monster. Humans welcome.
 
 Credits: 
 <div>Icons made by <a href="https://www.flaticon.com/authors/darius-dan" title="Darius Dan">Darius Dan</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div>
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,175 @@
-var friends = [{
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+var friends = [
+  {
+  "name":"Bigfoot",
+  "photo":"https://image.businessinsider.com/5a8c83d342e1cc57810ba9ee?width=900&format=jpeg&auto=webp",
+  "bio": "I'm very shy, but I have a big heart",
+  "scores":[
+      1,
+      3,
+      4,
+      5,
+      4,
+      1,
+      1,
+      3,
+      3,
+      4
+    ]
+},
+{
+  "name":"Nessie",
+  "photo":"https://www.thesun.co.uk/wp-content/uploads/2016/06/nintchdbpict000003016012.jpg?w=960",
+  "bio": "I have a lot of depth. (:",
+  "scores":[
+      5,
+      3,
+      4,
+      1,
+      1,
+      1,
+      2,
+      1,
+      2,
+      3
+    ]
+},
+{
+  "name":"The Kraken",
+  "photo":"https://undergroundscience.net/wp-content/uploads/2018/03/kraken-sea-monster1.jpg",
+  "bio": "I love hugs!",
   "scores":[
       5,
       1,
+      1,
+      1,
       4,
+      3,
+      2,
+      3,
+      5,
+      3
+    ]
+},
+{
+  "name":"Godzilla",
+  "photo":"https://cdn.onebauer.media/one/empire-images/reviews_films/5ced6e7e133d503e3a4a4685/godzilla-1.jpg?quality=50&width=1300&ratio=16-9&resizeStyle=aspectfill&format=jpg",
+  "bio": "I can protect you from Mothra.",
+  "scores":[
+      3,
+      3,
       4,
+      1,
+      5,
+      4,
+      1,
+      5,
+      2,
+      5
+    ]
+},
+{
+  "name":"Mothman",
+  "photo":"http://r.ddmcdn.com/s_f/o_1/APL/uploads/2014/07/mothman.jpg",
+  "bio": "Please be patient with my shyness!",
+  "scores":[
+      1,
+      5,
+      5,
       5,
       1,
-      2,
-      5,
+      1,
+      3,
       4,
+      2,
+      5
+    ]
+},
+{
+  "name":"Yeti",
+  "photo":"http://www.tntmagazine.com/image.php/media/content/_master/46706/images/yeti.jpg?file=media%2Fcontent%2F_master%2F46706%2Fimages%2Fyeti.jpg&width=450",
+  "bio": "I keep it chill.",
+  "scores":[
+      1,
+      2,
+      3,
+      4,
+      3,
+      2,
+      2,
+      3,
+      3,
+      4
+    ]
+},
+{
+  "name":"Jersey Devil",
+  "photo":"http://www.newanimal.org/jersey-devil.jpg",
+  "bio": "I'm a thrillseeker!",
+  "scores":[
+      1,
+      2,
+      4,
+      2,
+      2,
+      4,
+      4,
+      3,
+      3,
+      5
+    ]
+},
+{
+  "name":"Dracula",
+  "photo":"https://nationalpostcom.files.wordpress.com/2017/03/bela.jpg?quality=80&strip=all&w=780",
+  "bio": "Blah!",
+  "scores":[
+      2,
+      1,
+      5,
+      2,
+      1,
+      4,
+      5,
+      5,
+      2,
       1
+    ]
+},
+{
+  "name":"The Dover Demon",
+  "photo":"https://vignette.wikia.nocookie.net/cryptidz/images/4/47/D_demon.jpg/revision/latest?cb=20130902193046",
+  "bio": "I like long walks in the woods.",
+  "scores":[
+      1,
+      4,
+      5,
+      5,
+      1,
+      1,
+      4,
+      2,
+      4,
+      4
+    ]
+},
+{
+  "name":"The Flatwoods Monster",
+  "photo":"https://www.terraobscura.net/uploads/2/5/2/4/2524420/published/flatwoods_1.jpeg?1520524391",
+  "bio": "I have problems opening up.",
+  "scores":[
+      2,
+      3,
+      5,
+      2,
+      1,
+      4,
+      3,
+      4,
+      3,
+      4
     ]
 }
 ]
+
 
 module.exports = friends;
